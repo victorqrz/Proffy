f7172604203d8bb78a03d9fc7c348cc93b063242
Victor <victoorqrz@gmail.com>
2020-08-28 20:04:55 -0300

    finalizando pagina de listagem de profs

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,12 +2,60 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Estes são os proffys disponíveis."/>
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" id="week_day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars1.githubusercontent.com/u/38059087?s=460&v=4" alt="Avatar Github"/>
+            <div>
+              <strong>Gio Palm</strong>
+              <span>Programação Imperativa</span>
+            </div>
+          </header>
+          
+          <p>
+            Apaixonado pela linguagem Dafny e outras paradas. 
+            <br/><br/>
+            Aulas inovadoras e projetos um tanto quanto "fáceis".
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$45,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
